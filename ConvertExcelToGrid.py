bd2e60bb5c68e38ae7acb6a89dcead3e0f9e32d3
PTST <patrick.steffensen+git@gmail.com>
2017-09-29 10:05:24 +0200

    v0.1

--- ConvertExcelToGrid.py
+++ ConvertExcelToGrid.py
@@ -2,12 +2,20 @@ import openpyxl
 from pathfinding.core.diagonal_movement import DiagonalMovement
 from pathfinding.core.grid import Grid
 from pathfinding.finder.a_star import AStarFinder
+import sys
 
 '''
 OQ
 '''
+os = sys.platform
+print(os)
+if os == "win32":
+    wb = openpyxl.load_workbook('M:\\GitHub\\find_your_meeting\\groundFloor.xlsx')
+elif os == "darwin":
+    wb = openpyxl.load_workbook('/Users/PTST/Dev/find_your_meeting/groundFloor.xlsx')
+else:
+    raise ValueError("OS is not set")
 
-wb = openpyxl.load_workbook('/Users/PTST/Dev/find_your_meeting/groundFloor.xlsx')
 sheet = wb.get_sheet_by_name(wb.get_sheet_names()[0])
 maxCell = openpyxl.utils.column_index_from_string("OQ") 
 
@@ -21,7 +29,7 @@ for y in range(1, maxCell+1):
             x_list.append(1)
         elif sheet.cell(row=y, column=x).value != "":
             x_list.append(0)
-            location_dict[sheet.cell(row=y, column=x).value] = [x, y]
+            location_dict[sheet.cell(row=y, column=x).value] = [x-1, y-1]
         else:
             x_list.append(0)
     master_list.append(x_list)
@@ -31,8 +39,8 @@ for key, value in location_dict.items() :
 
 grid = Grid(matrix=master_list)
 
-startNode = "h1.25d"
-endNode = "kantinen"
+startNode = input("Start: ")
+endNode = input("End: ")
 
 location_dict[startNode][0], location_dict[startNode][1]
 
@@ -46,5 +54,12 @@ print('operations:', runs, 'path length:', len(path))
 for item in path:
     sheet.cell(row=(item[1]+1), column=(item[0]+1)).value = "x"
 
-wb.save('/Users/PTST/Dev/find_your_meeting/path.xlsx')
+
+if os == "win32":
+    wb.save('M:\\GitHub\\find_your_meeting\path.xlsx')
+elif os == "darwin":
+    wb.save('/Users/PTST/Dev/find_your_meeting/path.xlsx')
+else:
+    raise ValueError("OS is not set")
+
 
