fd6fb181a8e7537aa63477ba8613222109fab94b
PTST <patrick.steffensen+git@gmail.com>
2017-09-29 20:29:47 +0200

    OK

--- ConvertExcelToGrid.py
+++ ConvertExcelToGrid.py
@@ -7,7 +7,7 @@ import os
 import copy
 
 """
-OQ
+Loads excel file into memory, extracts the first sheet as an object, and returns the numerical value of 'lastColumn'
 """
 def load_excel(lastColumn):
     operating_system = sys.platform
@@ -19,35 +19,49 @@ def load_excel(lastColumn):
         raise ValueError("Unknown operating system")
     return wb, wb.get_sheet_by_name(wb.get_sheet_names()[0]), openpyxl.utils.column_index_from_string(lastColumn)
 
-def convert_to_grid(sheet, last_cell):
+'''
+Converts excel sheet to a square grid with the dimension last_cell x last_cell
+'''
+def excel_to_list(sheet, last_cell):
     master_list = []
     location_dict = {}
+    up_down_dict = {}
     for y in range(1, last_cell+1):
         x_list = []
         for x in range(1, last_cell+1):
-            if sheet.cell(row=y, column=x).value == ".":
+            cell_value = sheet.cell(row=y, column=x).value
+            if cell_value == ".":
                 x_list.append(0)
-            elif sheet.cell(row=y, column=x).value is not None:
-                location_dict[str(sheet.cell(row=y, column=x).value).lower()] = [x-1, y-1]
+            elif cell_value is not None:
+                if (cell_value.upper().startswith("UP")) or (cell_value.upper().startswith("DOWN")):
+                    up_down_dict[str(cell_value).upper()] = [x-1, y-1]
+                    location_dict[str(cell_value).upper()] = [x-1, y-1]
+                else:
+                    location_dict[str(cell_value).upper()] = [x-1, y-1]
                 x_list.append(0)
             else:
                 x_list.append(1)
         master_list.append(x_list)
-    grid = Grid(matrix=master_list)
-    return grid, location_dict
+    
+    return master_list, location_dict, up_down_dict
 
+'''
+Prints available location from dict
+'''
 def print_locations(location_dict):
     print("\nPossible locations:")
-    for key, value in location_dict.items() :
-        print (key, value)
-
-
-def find_path(location_dict, grid):
+    for key, value in location_dict.items():
+        if not (key.startswith("UP")) or (key.startswith("DOWN")):
+            print (key, value)
+'''
+Get start and endpoint coordinates
+'''
+def get_coordinates(location_dict):
     while True:
         while True:
             try:
-                start_node = input("\nStart: ").lower()
-                start = grid.node(location_dict[start_node][0], location_dict[start_node][1])
+                start_point = input("\nStart: ").upper()
+                test = location_dict[start_point][0]
                 break
             except KeyError:
                 print("\nNot a valid starting point. Try again")
@@ -55,25 +69,34 @@ def find_path(location_dict, grid):
 
         while True:
             try:
-                end_node = input("\nEnd: ").lower()
-                end = grid.node(location_dict[end_node][0], location_dict[end_node][1])
+                end_point = input("\nEnd: ").upper()
+                test2 = location_dict[end_point][0]
                 break
             except KeyError:
                 print("\nNot a valid endpoint. Try again")
                 print_locations(location_dict)
-        if end == start:
+        if test == test2:
             print("Starting point and endpoint is the same")
         else:
             break
+    return start_point, end_point
 
+'''
+Finds optimal path between locations via a* algorithm
+'''
+def find_path(location_dict, start_point, end_point, grid):
+    start = grid.node(location_dict[start_point][0], location_dict[start_point][1])
+    end = grid.node(location_dict[end_point][0], location_dict[end_point][1])
     finder = AStarFinder(diagonal_movement=DiagonalMovement.never)
     path, runs = finder.find_path(start, end, grid)
 
     print("operations:", runs, "path length:", len(path))
     return path
 
-
-def save_to_excel(wb, path, sheet):
+'''
+saves excel workbook to file with optimal path highlighted
+'''
+def save_to_excel(wb, path, sheet, open_excel=True):
     operating_system = sys.platform
     for item in path:
         sheet.cell(row=(item[1]+1), column=(item[0]+1)).value = "x"
@@ -81,27 +104,46 @@ def save_to_excel(wb, path, sheet):
         sheet.cell(row=(item[1]+1), column=(item[0]+1)).fill = openpyxl.styles.PatternFill(fgColor=openpyxl.styles.colors.RED, fill_type = "solid")
     if operating_system == "win32":
         wb.save("M:\\GitHub\\find_your_meeting\path.xlsx")
+        if open_excel:
+            os.startfile("M:\\GitHub\\find_your_meeting\path.xlsx")
     elif operating_system == "darwin":
         wb.save("/Users/PTST/Dev/find_your_meeting/path.xlsx")
+        if open_excel:
+            os.system("open /Users/PTST/Dev/find_your_meeting/path.xlsx")
     else:
         raise ValueError("Unknown operating system")
 
-    os.startfile("M:\\GitHub\\find_your_meeting\path.xlsx")
 
-
-last_column=input("Last column: ").upper()
-workbook, excel_sheet, max_ccell = load_excel(last_column)
+'''
+Execution of functions
+'''
+#last_column=input("Last column: ").upper()
+last_column = "OQ"
+workbook, excel_sheet, max_cell = load_excel(last_column)
 orig_workbook = copy.copy(workbook)
-search_grid, possible_locations = convert_to_grid(excel_sheet, max_ccell)
-do = True
-while do:
-    print_locations(possible_locations)
-    found_path = find_path(possible_locations, search_grid)
-    save_to_excel(workbook, found_path, excel_sheet)
-    while True:
-        answer = input("Run again (Y/N): ").lower()
-        if (answer == "y") or (answer == "n"):
-            do = (answer == "y")
-            workbook, excel_sheet, max_ccell = load_excel(last_column)
-            break
+master_list, possible_locations, up_down_locations = excel_to_list(excel_sheet, max_cell)
+search_grid = Grid(matrix=master_list)
 
+
+possible_locations["H2.08"] = [200,300]
+
+
+print_locations(possible_locations)
+start_node, end_node = get_coordinates(possible_locations)
+path_dict_start = {}
+if start_node[1:2] != end_node[1:2]:
+    if start_node[1:2] < end_node[1:2]:
+        direction = "UP"
+    else:
+        direction = "DOWN"
+    for key, value in up_down_locations.items():
+        if key.startswith(direction):
+            found_path = find_path(possible_locations, start_node, key, search_grid)
+            path_dict_start[key] = found_path
+            search_grid = Grid(matrix=master_list)
+else:
+    found_path = find_path(possible_locations, start_node, end_node, search_grid)
+
+short_key = min(path_dict_start, key=path_dict_start.get)
+print(short_key + " - " + str(len(path_dict_start[short_key])))
+#save_to_excel(workbook, found_path, excel_sheet, False)
--- convertPng.py
+++ convertPng.py
@@ -4,7 +4,7 @@ import openpyxl
 wb = openpyxl.load_workbook('/Users/PTST/Dev/find_your_meeting/test.xlsx')
 sheet = wb.get_sheet_by_name(wb.get_sheet_names()[0])
 
-im = Image.open("/Users/PTST/Dev/find_your_meeting/Ground Floor.png") #Can be many different formats.
+im = Image.open("/Users/PTST/Dev/find_your_meeting/basement.png") #Can be many different formats.
 pix = im.load()
 img_x, img_y = tuple(map(int, im.size))
 
@@ -13,5 +13,5 @@ print(pix[5,5] != white)
 for y in range(0, img_y, 3):
     for x in range(0, img_x, 3):
         if pix[x,y] != white:
-            sheet.cell(row=int((y+1)/3), column=int((x+1)/3)).fill = openpyxl.styles.PatternFill(bgColor="FFFFFF", fill_type = "solid")
-wb.save('/Users/PTST/Dev/find_your_meeting/testOut3.xlsx')
+            sheet.cell(row=int((y+1)/3), column=int((x+1)/3)).fill = openpyxl.styles.PatternFill(fgColor="000000", fill_type = "solid")
+wb.save('/Users/PTST/Dev/find_your_meeting/basement.xlsx')
